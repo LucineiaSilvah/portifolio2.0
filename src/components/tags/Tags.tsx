579c8285f4e9073827d0bf4ab33eb5a002d233dc
Lucineia Silva <lucineia.rsilvah@gmail.com>
2026-05-14 21:27:02 -0300

    ajuste no tamanho da tag

--- src/components/tags/Tags.tsx
+++ src/components/tags/Tags.tsx
@@ -6,7 +6,7 @@ interface  Props{
 
 const Tags = ({children }:Props) => {
   return (
-    <div className="min-w-20 h-12 lg:h-8  flex justify-center items-center p-2 shadow-sm shadow-gray-400 rounded-full font-50 font-sans border bg-gray-100/70 font-100">
+    <div className="capitalize  min-w-10 lg:w-60 h-6 my-2 text-sm lg:text-lg lg:h-10  flex justify-center items-center p-2 shadow-sm shadow-gray-400 rounded-full font-50 font-sans border bg-gray-100/70 font-100">
       {children}
     </div>
   );
